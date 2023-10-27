4e9c14c63c4154af3cd0434265dcf95a66483cce
Indie Free <realgodd@yandex.ru>
2023-10-27 17:34:05 +0700

    start

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 window.onload = function() {
 	var langList = ["RU", "ENG"];
 	var config = {
-		lang: "RU"
+		lang: "RU",
+		showMode: "off"
 	};
+
+	$('header').on('click', function(){
+		if (config.showMode == "off") {
+			config.showMode = "on";
+			$('#nav_ul').show();
+		} else {
+			config.showMode = "off";
+			$('#nav_ul').hide();
+		};
+	});
 };
