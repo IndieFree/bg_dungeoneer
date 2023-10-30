e508ab33d94e33bc15d021830ffac4ed735c7525
Indie Free <realgodd@yandex.ru>
2023-10-30 21:12:03 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 window.onload = function() {
+	var data = {
+		hero: [
+			{id: 1, name: "Лорел Аланис", hp: 6, str: 0, mana: 1, agil: 2, dop: 1},
+			{id: 2, name: "Рака", hp: 6, str: 1, mana: 0, agil: 2, dop: 1},
+			{id: 3, name: "Родерик Талус", hp: 6, str: 1, mana: 0, agil: 2, dop: 1},
+			{id: 4, name: "Сибот", hp: 6, str: 0, mana: 1, agil: 2, dop: 1},
+			{id: 5, name: "Корн Кромсатель", hp: 6, str: 1, mana: 0, agil: 2, dop: 1},
+			{id: 6, name: "Тейнин Легкоступ", hp: 6, str: 1, mana: 0, agil: 2, dop: 1},
+		]
+	};
 	var langList = ["RU", "ENG"];
 	var config = {
 		lang: "RU",
