52955d2a7d4dd4143a3b7e1c516e4e9c0b2ee848
Indie Free <realgodd@yandex.ru>
2023-10-31 09:50:31 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,6 @@
-window.onload = function() {
-	var mainData = {
-		dops: {
-			1: "Гробница повелителя мертвых",
-			2: "Царство Ледяной ведьмы"
-		},
-		hero: [
-			{id: 1, name: "Лорел Аланис", hp: 6, str: 0, mana: 1, agil: 2, dop: 1},
-			{id: 2, name: "Рака", hp: 6, str: 1, mana: 0, agil: 2, dop: 1},
-			{id: 3, name: "Родерик Талус", hp: 6, str: 1, mana: 0, agil: 2, dop: 1},
-			{id: 4, name: "Сибот", hp: 6, str: 0, mana: 1, agil: 2, dop: 1},
-			{id: 5, name: "Корн Кромсатель", hp: 6, str: 1, mana: 0, agil: 2, dop: 1},
-			{id: 6, name: "Тейнин Легкоступ", hp: 6, str: 1, mana: 0, agil: 2, dop: 1},
-			{id: 7, name: "Бодонс Винг", hp: 6, str: 1, mana: 0, agil: 2, dop: 2},
-			{id: 8, name: "Надалия", hp: 6, str: 0, mana: 1, agil: 2, dop: 2},
-			{id: 9, name: "Тэвин Дрексис", hp: 6, str: 1, mana: 0, agil: 2, dop: 2},
-			{id: 10, name: "Оноку Кай", hp: 6, str: 0, mana: 1, agil: 2, dop: 2},
-			{id: 11, name: "Ригус Гар", hp: 6, str: 1, mana: 0, agil: 2, dop: 2},
-			{id: 12, name: "Энрака Тивус", hp: 6, str: 1, mana: 0, agil: 2, dop: 2}			
-		]
-	};
+var mDATA = mainData;
 
-	console.log('mainData');
-	console.log(mainData);
-	
+window.onload = function() {
 	var langList = ["RU", "ENG"];
 	var config = {
 		lang: "RU",
@@ -30,8 +8,36 @@ window.onload = function() {
 		section: "mp_aboutGame"
 	};
 
+	// на старте отображаем форму "об игре"
 	$('#mp_aboutGame').show();
 
+	// скрыть все контейнеры, отобразить только "нужный"
+	function add_hideAll_MP(showElem) {
+		$('#mp_aboutGame').hide();
+		$('#mp_helper').hide();
+		$('#mp_rules').hide();
+		$('#mp_quest').hide();
+		$('#mp_goods').hide();
+		$('#mp_heroes').hide();
+		let elemId = showElem.attr('id');
+		if (elemId == 'mp_aboutGame') {
+			$('#nav_header').html("| Об игре");
+		} else if (elemId == 'mp_helper') {
+			$('#nav_header').html("| Карта помощи");
+		} else if (elemId == 'mp_rules') {
+			$('#nav_header').html("| Правила игры");
+		} else if (elemId == 'mp_quest') {
+			$('#nav_header').html("| Задания");
+		} else if (elemId == 'mp_goods') {
+			$('#nav_header').html("| Предметы");
+		} else {
+			$('#nav_header').html("| Герои");
+		};
+		
+		showElem.show();
+	};	
+
+	// скрыть/показать шапку
 	$('header').on('click', function(){
 		if (config.showMode == "off") {
 			config.showMode = "on";
@@ -42,6 +48,26 @@ window.onload = function() {
 			$('header').height('5vh');
 			$('#nav_ul').hide();
 		};
+	});
+
+	// обработчики нажатия на кнопки в шапке
+	$('#bttn_aboutGame').on('click', function(){
+		add_hideAll_MP($('#mp_aboutGame'));
+	});	
+	$('#bttn_helper').on('click', function(){
+		add_hideAll_MP($('#mp_helper'));
+	});	
+	$('#bttn_rules').on('click', function(){
+		add_hideAll_MP($('#mp_rules'));
+	});	
+	$('#bttn_quest').on('click', function(){
+		add_hideAll_MP($('#mp_quest'));
+	});	
+	$('#bttn_goods').on('click', function(){
+		add_hideAll_MP($('#mp_goods'));
+	});	
+	$('#bttn_heroes').on('click', function(){
+		add_hideAll_MP($('#mp_heroes'));
 	});
 
 
